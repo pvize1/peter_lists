ee4057077a4105bef70a97e006b18f4ec07ea715
Peter Vize <4411615+pvize1@users.noreply.github.com>
2021-02-14 20:20:19 +0000

    Added slug to books model

--- peter_lists/books/models.py
+++ peter_lists/books/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 
 class Book(TimeStampedModel):
     title = models.CharField("Title of Book", max_length=255)
+    slug = models.SlugField(null=False, unique=True, default="_")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("book_detail", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
