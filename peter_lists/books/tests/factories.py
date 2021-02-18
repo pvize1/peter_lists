e67cfadbf56484eb35a99cf6ec251651d14c6771
Peter Vize <4411615+pvize1@users.noreply.github.com>
2021-02-18 20:46:54 +0000

    Added BookFactory for testing, __Str__ tests for all models successful TODO: 2 functions still not covered by tests

---
+++ peter_lists/books/tests/factories.py
@@ -0,0 +1,42 @@
+from django.template.defaultfilters import slugify
+
+from factory.django import DjangoModelFactory
+from factory import Faker, fuzzy, LazyAttribute, SubFactory
+
+from ..models import Book, Author, Publisher, BookType
+
+
+class AuthorFactory(DjangoModelFactory):
+    class Meta:
+        model = Author
+
+    name = fuzzy.FuzzyText()
+
+
+class PublisherFactory(DjangoModelFactory):
+    class Meta:
+        model = Publisher
+
+    name = fuzzy.FuzzyText()
+
+
+class BookTypeFactory(DjangoModelFactory):
+    class Meta:
+        model = BookType
+
+    type = fuzzy.FuzzyText()
+
+
+class BookFactory(DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    title = fuzzy.FuzzyText()
+    subtitle = fuzzy.FuzzyText()
+    author = SubFactory(AuthorFactory)
+    description = Faker("paragraph", nb_sentences=3, variable_nb_sentences=True)
+    isbn = fuzzy.FuzzyText()
+    publisher = SubFactory(PublisherFactory)
+    pages = 99
+    type = SubFactory(BookTypeFactory)
+    slug = LazyAttribute(lambda obj: slugify(obj.title))
---
+++ peter_lists/books/tests/test_models.py
@@ -0,0 +1,33 @@
+import pytest
+from peter_lists.books.tests.factories import (
+    BookFactory,
+    AuthorFactory,
+    PublisherFactory,
+    BookTypeFactory,
+)
+
+pytestmark = pytest.mark.django_db
+
+
+def test_author__str__():
+    author = AuthorFactory()
+    assert author.__str__() == author.name
+    assert str(author) == author.name
+
+
+def test_publisher__str__():
+    publisher = PublisherFactory()
+    assert publisher.__str__() == publisher.name
+    assert str(publisher) == publisher.name
+
+
+def test_booktype__str__():
+    booktype = BookTypeFactory()
+    assert booktype.__str__() == booktype.type
+    assert str(booktype) == booktype.type
+
+
+def test_book__str__():
+    book = BookFactory()
+    assert book.__str__() == book.title
+    assert str(book) == book.title
