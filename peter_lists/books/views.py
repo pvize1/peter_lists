35b5e6bdb1f8f9235379b5b7b370cb026eafe494
Peter Vize <4411615+pvize1@users.noreply.github.com>
2021-02-19 19:16:59 +0000

    Added template names, so that pycharm can do go-to in gutter

--- peter_lists/books/views.py
+++ peter_lists/books/views.py
@@ -3,7 +3,9 @@ from .models import Book
 
 class BookListView(ListView):
     model = Book
+    template_name = "../templates/books/book_list.html"
 
 class BookDetailView(DetailView):
     model = Book
+    template_name = "../templates/books/book_detail.html"
 
