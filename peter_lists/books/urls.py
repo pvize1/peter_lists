74201e105ea1fc4c98e0d2e6033c8fdbc8dfacf8
Peter Vize <4411615+pvize1@users.noreply.github.com>
2021-02-19 19:43:59 +0000

    Added create/add view and template

--- peter_lists/books/urls.py
+++ peter_lists/books/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = "books"
-urlpatterns = [path(route='', view=views.BookListView.as_view(), name='list'),
-               path(route ='<slug:slug>/', view=views.BookDetailView.as_view(), name="detail"), ]
+urlpatterns = [
+    path(route="", view=views.BookListView.as_view(), name="list"),
+    path(route="add/", view=views.BookCreateView.as_view(), name="add"),
+    path(route="<slug:slug>/", view=views.BookDetailView.as_view(), name="detail"),
+]
--- peter_lists/books/views.py
+++ peter_lists/books/views.py
@@ -1,11 +1,26 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Book
+
 
 class BookListView(ListView):
     model = Book
     template_name = "../templates/books/book_list.html"
 
+
 class BookDetailView(DetailView):
     model = Book
     template_name = "../templates/books/book_detail.html"
 
+
+class BookCreateView(CreateView):
+    model = Book
+    template_name = "../templates/books/book_form.html"
+    fields = [
+        "title",
+        "subtitle",
+        "description",
+        "author",
+        "type",
+        "form",
+        "status",
+    ]
