9ba2ebe011a2e7ea61246df5050013da4e674692
Peter Vize <4411615+pvize1@users.noreply.github.com>
2021-03-31 14:14:36 +0100

    Changed default mailgun url

--- config/settings/production.py
+++ config/settings/production.py
@@ -102,7 +102,7 @@ EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 ANYMAIL = {
     "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
     "MAILGUN_SENDER_DOMAIN": env("MAILGUN_DOMAIN"),
-    "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.eu.mailgun.net/v3/mg.petervize.co.uk"),
+    "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.eu.mailgun.net/v3"),
 }
 
 
