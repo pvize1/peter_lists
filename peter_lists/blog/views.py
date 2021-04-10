297e9e9566361da1e7bc5c73465ad343e159f1ac
Peter Vize <4411615+pvize1@users.noreply.github.com>
2021-04-10 19:45:49 +0100

    Fixed top n tags code for topn=0

--- peter_lists/blog/views.py
+++ peter_lists/blog/views.py
@@ -13,23 +13,29 @@ from .models import Blog
 from .forms import EditBlogForm
 
 
-def TagCount(topn=999):
+def tag_count(topn=0):
     raw_tags = Blog.blog.values("tag").annotate(count=Count("tag"))
     count_tags = dict()
     for record in raw_tags:
         for tag in record["tag"].split(","):
             k = tag.strip().lower()
             count_tags[k] = count_tags.get(k, 0) + record["count"]
-    return {
-        k: count_tags[k]
-        for k in sorted(count_tags, key=count_tags.get, reverse=True)[:topn]
-    }
+    if topn == 0:
+        return {
+            k: count_tags[k]
+            for k in sorted(count_tags, key=count_tags.get, reverse=True)
+        }
+    else:
+        return {
+            k: count_tags[k]
+            for k in sorted(count_tags, key=count_tags.get, reverse=True)[:topn]
+        }
 
 
 # Create your views here.
 def BlogHome(request):
     blogs = Blog.blog.order_by("-modified")[:3]
-    tag_sorted = TagCount(topn=5)
+    tag_sorted = tag_count(topn=5)
     return render(request, "blog/blog_home.html", {"blogs": blogs, "tags": tag_sorted})
 
 
@@ -40,7 +46,7 @@ class BlogListView(ListView):
 
 
 def BlogAllTagsView(request):
-    tag_sorted = TagCount()
+    tag_sorted = tag_count()
     return render(request, "blog/blog_tags.html", {"tags": tag_sorted})
 
 
