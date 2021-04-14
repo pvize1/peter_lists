f34f31e471b8097ba552caf1e4e9384b68cb0585
Peter Vize <4411615+pvize1@users.noreply.github.com>
2021-04-14 21:17:53 +0100

    Added many-to-many images to Blog

--- peter_lists/blog/models.py
+++ peter_lists/blog/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 from django.urls import reverse
+from ..images.models import Image
 
 
 # Create your models here.
+class ImageList(TimeStampedModel):
+    blogs = models.ForeignKey("Blog", on_delete=models.CASCADE)
+    blog_image = models.ForeignKey("images.Image", on_delete=models.CASCADE)
+
+
 class Blog(TimeStampedModel):
     title = models.CharField("Title", max_length=250)
     description = models.TextField("Description", blank=True)
     date = models.DateTimeField("Date")
     tag = models.CharField("Tag", max_length=100, blank=True, default="none, ")
+    images = models.ManyToManyField(
+        Image,
+        through=ImageList,
+        related_name="blog_image",
+    )
 
     class Meta:
         ordering = ["-date", "title"]
