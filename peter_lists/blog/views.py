db547d286dd416256db66f3c38611a7c1e9e4c2c
Peter Vize <4411615+pvize1@users.noreply.github.com>
2021-04-16 11:25:51 +0100

    Adding permissions to blog

--- peter_lists/blog/views.py
+++ peter_lists/blog/views.py
@@ -17,7 +17,11 @@ from .forms import EditBlogForm
 
 def tag_count(topn=0):
     # TODO Move to model manager
-    raw_tags = Blog.blog.order_by("tag").values("tag").annotate(count=Count("tag"), tag_new=Trim(Lower("tag")))
+    raw_tags = (
+        Blog.blog.order_by("tag")
+        .values("tag")
+        .annotate(count=Count("tag"), tag_new=Trim(Lower("tag")))
+    )
     count_tags = dict()
     for record in raw_tags:
         for tag in record["tag_new"].split(","):
@@ -36,6 +40,7 @@ def tag_count(topn=0):
             for k in sorted(count_tags, key=count_tags.get, reverse=True)[:topn]
         }
 
+
 # Create your views here.
 def BlogHome(request):
     blogs = Blog.blog.order_by("-modified")[:3]
@@ -47,7 +52,7 @@ class BlogListView(PermissionRequiredMixin, ListView):
     model = Blog
     paginate_by = 3
     template_name = "blog/blog_list.html"
-    permission_required = "Blog.can_view"
+    permission_required = "blog.view_blog"
 
 
 def BlogAllTagsView(request):
@@ -60,7 +65,7 @@ class BlogTagListView(PermissionRequiredMixin, ListView):
     model = Blog
     paginate_by = 3
     template_name = "blog/blog_list.html"
-    permission_required = "Blog.can_view"
+    permission_required = "blog.view_blog"
 
     def get_queryset(self):
         return Blog.blog.filter(tag__contains=self.kwargs["tag_name"])
@@ -69,23 +74,26 @@ class BlogTagListView(PermissionRequiredMixin, ListView):
 class BlogDetailView(PermissionRequiredMixin, DetailView):
     model = Blog
     template_name = "blog/blog_detail.html"
-    permission_required = "Blog.can_view"
+    permission_required = "blog.view_blog"
 
 
-class BlogCreateView(LoginRequiredMixin, CreateView):
+class BlogCreateView(PermissionRequiredMixin, LoginRequiredMixin, CreateView):
     form_class = EditBlogForm
     model = Blog
     action = "Add"
     template_name = "blog/blog_form.html"
+    permission_required = "blog.add_blog"
 
 
-class BlogUpdateView(LoginRequiredMixin, UpdateView):
+class BlogUpdateView(PermissionRequiredMixin, LoginRequiredMixin, UpdateView):
     form_class = EditBlogForm
     model = Blog
     action = "Edit"
     template_name = "blog/blog_form.html"
+    permission_required = "blog.change_blog"
 
 
-class BlogDeleteView(LoginRequiredMixin, DeleteView):
+class BlogDeleteView(PermissionRequiredMixin, LoginRequiredMixin, DeleteView):
     model = Blog
     success_url = reverse_lazy("blog:list")
+    permission_required = "blog.delete_blog"
